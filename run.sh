a68767a109fe17621f605f92f40277fe62d52db8
Ubuntu <ubuntu@ip-172-31-32-130.ec2.internal>
2017-11-01 03:03:49 +0000

    version0.30

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+cd /home/ubuntu/Desktop/ECE1779-A2-worker/ECE1779A2
 ./venv/bin/gunicorn --bind 0.0.0.0:80 --workers=8 --worker-class gevent --access-logfile access.log --error-logfile error.log app:webapp
